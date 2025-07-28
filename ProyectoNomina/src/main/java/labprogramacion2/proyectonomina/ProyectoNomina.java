939a2d1431d555035e558416cbf8be29f2b5db47
Duanel <du4n31@gmail.com>
2025-07-28 09:15:12 -0400

    Creación del Proyecto.

---
+++ ProyectoNomina/src/main/java/labprogramacion2/proyectonomina/ProyectoNomina.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package labprogramacion2.proyectonomina;
+
+/**
+ *
+ * @author Duanel
+ */
+public class ProyectoNomina {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
